54cf0df50a7d6f9c0d10625aa8778dbf93010c61
MWMW1133 <134836235+MWMW1133@users.noreply.github.com>
2025-08-07 17:47:46 +0900

    c

--- dairy/src/main/java/com/share/dairy/BuddyDiaryController.java
+++ dairy/src/main/java/com/share/dairy/BuddyDiaryController.java
@@ -1,15 +1,15 @@
-package com.dairy.dairy;
+package com.share.dairy;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class SettingsController {
+public class BuddyDiaryController {
 
     @FXML
     private Label titleLabel;
 
     @FXML
     public void initialize() {
-        System.out.println("SettingsController 초기화됨");
+        System.out.println("BuddyDiaryController 초기화됨");
     }
 }
