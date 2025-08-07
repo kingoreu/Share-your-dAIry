f56f71d69c008ec8126341f56538de23cdd73956
MWMW1133 <134836235+MWMW1133@users.noreply.github.com>
2025-08-07 13:39:03 +0900

    틀
    틀

---
+++ dairy/src/main/java/com/dairy/dairy/MainController.java
@@ -0,0 +1,45 @@
+package com.dairy.dairy;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+
+public class MainController {
+
+    @FXML
+    private StackPane contentArea;
+
+    @FXML
+    private void showHome() throws IOException {
+        setContent("home-view.fxml");
+    }
+
+    @FXML
+    private void showMyDiary() throws IOException {
+        setContent("my-diary-view.fxml");
+    }
+
+    @FXML
+    private void showOurDiary() throws IOException {
+        setContent("our-diary-view.fxml");
+    }
+
+    @FXML
+    private void showBuddyDiary() throws IOException {
+        setContent("buddy-diary-view.fxml");
+    }
+
+    @FXML
+    private void showSettings() throws IOException {
+        setContent("settings-view.fxml");
+    }
+
+    private void setContent(String fxml) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxml));
+        Pane newPane = loader.load();
+        contentArea.getChildren().setAll(newPane);
+    }
+}
---
+++ dairy/src/main/java/com/dairy/dairy/OurDiaryController.java
@@ -0,0 +1,15 @@
+package com.dairy.dairy;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class OurDiaryController {
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    public void initialize() {
+        System.out.println("OurDiaryController 초기화됨");
+    }
+}
