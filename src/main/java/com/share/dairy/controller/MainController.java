3759714243051031cb95e47c26448823d50c11df
MWMW1133 <134836235+MWMW1133@users.noreply.github.com>
2025-08-11 12:50:24 +0900

    diary 경로 수정
    mainController diary 경로 수정

--- src/main/java/com/share/dairy/controller/MainController.java
+++ src/main/java/com/share/dairy/controller/MainController.java
@@ -49,9 +49,9 @@ public class MainController {
 
     @FXML private void onWardrobeClicked(MouseEvent e)   { /* TODO */ }
     @FXML private void onWindowClicked(MouseEvent e)     { loadView("/fxml/moodGraph/mood-graph-view.fxml"); }
-    @FXML private void onLaptopClicked(MouseEvent e)     { loadView("/fxml/my_diary/my-diary-view.fxml"); }
+    @FXML private void onLaptopClicked(MouseEvent e)     { loadView("/fxml/diary/my_diary/my-diary-view.fxml"); }
     @FXML private void onBookshelfClicked(MouseEvent e)  { /* TODO */ }
-    @FXML private void onRadioClicked(MouseEvent e)      { loadView("/fxml/our_diary/home-view.fxml"); }
+    @FXML private void onRadioClicked(MouseEvent e)      { loadView("/fxml/diary/our_diary/home-view.fxml"); }
     @FXML private void onCharacterClicked(MouseEvent e)  { loadView("/fxml/userInfo/settings-view.fxml"); }
 
     private void loadView(String fxmlPath) {
