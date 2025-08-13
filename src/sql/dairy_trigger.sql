c3a8816a7a3308648cb963ef19e5305051a03f79
MWMW1133 <134836235+MWMW1133@users.noreply.github.com>
2025-08-13 13:46:51 +0900

    Update dairy_trigger.sql

--- src/sql/dairy_trigger.sql
+++ src/sql/dairy_trigger.sql
@@ -1,30 +1,26 @@
 USE dairy;
 
--- =========================================
--- 1) 필수 트리거
---      주로 데이터 정합성에 어긋날 경우를 고려하여 작성.
--- =========================================
-
--- 1-1. 자기 자신을 친구로 추가 금지
-DROP TRIGGER IF EXISTS trg_friendship_no_self;
+-- 1) 자기 자신을 친구로 추가 금지
+DROP TRIGGER IF EXISTS dairy.trg_friendship_no_self;
 DELIMITER //
-CREATE TRIGGER trg_set_updated_at_on_update
-	BEFORE UPDATE ON diary_entries
-    FOR EACH ROW
+CREATE TRIGGER dairy.trg_friendship_no_self
+BEFORE INSERT ON `friendship`
+FOR EACH ROW
 BEGIN
-    SET NEW.diary_updated_at = NOW();
-END;
-//
+    IF NEW.user_id = NEW.friend_id THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = '자기 자신을 친구로 추가할 수 없습니다.';
+    END IF;
+END//
 DELIMITER ;
 
--- 1-2. 다이어리 수정 시 diary_updated_at 자동 변경
-DROP TRIGGER IF EXISTS trg_set_updated_at_on_update;
+-- 2) 다이어리 수정 시 diary_updated_at 자동 갱신
+DROP TRIGGER IF EXISTS dairy.trg_set_updated_at_on_update;
 DELIMITER //
-CREATE TRIGGER trg_set_updated_at_on_update
-    BEFORE UPDATE ON diary_entries
-    FOR EACH ROW
+CREATE TRIGGER dairy.trg_set_updated_at_on_update
+BEFORE UPDATE ON diary_entries
+FOR EACH ROW
 BEGIN
     SET NEW.diary_updated_at = NOW();
-END;
-
+END//
 DELIMITER ;
